dd7a0ec6257fec87cc648701134de3040f2033a0
Michael J Huber Jr <info@op131.com>
2017-03-06 14:07:11 -0700

    connection to database passed

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "op131",
+    name: "op131Lehi",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
         .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1)
     ]
 )
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,9 +1,18 @@
 import Vapor
 import VaporMySQL
 
-let drop = droplet()
+let drop = Droplet()
 try drop.addProvider(VaporMySQL.Provider)
 
 // Preparations of your model here
+
+drop.get("version") { _ in
+    if let db = drop.database?.driver as? MySQLDriver {
+        let version = try db.raw("SELECT version()")
+        return try JSON(node: version)
+    } else {
+        return "Database Connection failed..."
+    }
+}
 
 drop.run()
