b4ec8bb4a2e9c85088802d2577d5711aca8defd6
Michael J Huber Jr <info@op131.com>
2017-03-07 14:38:48 -0700

    add fetchMessagesByUser route to UserController

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -6,6 +6,7 @@ final class UserController {
     func addRoutes(drop: Droplet) {
         let op131Lehi = drop.grouped("op131Lehi/users")
         op131Lehi.get("/", handler: fetchUsers)
+        op131Lehi.get("/", LehiUser.self, handler: fetchMessagesByUser)
         
         op131Lehi.post("register", handler: register)
     }
@@ -14,6 +15,11 @@ final class UserController {
     
     func fetchUsers(request: Request) throws -> ResponseRepresentable {
         return try LehiUser.all().makeResponse()
+    }
+    
+    func fetchMessagesByUser(request: Request, user: LehiUser) throws -> ResponseRepresentable {
+        let messages = try user.messages()
+        return try messages.makeResponse()
     }
     
     // MARK: - Post User
@@ -32,4 +38,12 @@ final class UserController {
 
     }
     
+}
+
+// MARK: - Messages
+
+extension LehiUser {
+    func messages() throws -> [Message] {
+        return try children(nil, Message.self).all()
+    }
 }
